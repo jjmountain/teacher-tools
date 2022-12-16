50983c159069f43d1afc9cb553018c77c169c20e
James Devereux <devereuxjj@gmail.com>
2022-12-16 18:09:47 +0900

    set up timer in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, Dispatch } from "react";
+import { PlayButton } from "./components/PlayButton";
+import { ResetButton } from "./components/ResetButton";
 
-function App() {
+import "./App.css";
+
+// types
+
+const initialState: TimerState = {
+  timerState: "stopped",
+  milliseconds: 0,
+};
+
+type TimerState = {
+  milliseconds: number;
+  timerState: "stopped" | "playing" | "paused";
+};
+
+type TimerAction = {
+  type: "PLAY" | "PAUSE" | "RESET";
+};
+
+type DisplayState = {
+  minutes: number;
+  seconds: number;
+};
+
+// create a timer that logs the time to the screen and has a start stop and reset button
+
+const reducer = (
+  state: typeof initialState,
+  action: TimerAction
+): TimerState => {
+  switch (action.type) {
+    case "PLAY":
+      return { milliseconds: 2, timerState: "stopped" };
+    case "PAUSE":
+      return { milliseconds: 2, timerState: "stopped" };
+    case "RESET":
+      return {
+        milliseconds: 0,
+        timerState: "stopped",
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const Display = ({ minutes, seconds }: DisplayState) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full flex justify-center h-24 mt-8 border-gray-500 ">
+      <div className="h-full w-1/3 border-2 border-gray-500 bg-white text-5xl flex items-center justify-center">
+        {minutes}:{seconds}
+      </div>
     </div>
   );
-}
+};
+
+const Controls = ({
+  dispatch,
+  state,
+}: {
+  state: TimerState;
+  dispatch: Dispatch<TimerAction>;
+}) => {
+  return (
+    <div className="flex h-full flex-row cursor-pointer items-center justify-center ">
+      <PlayButton />
+      <ResetButton />
+    </div>
+  );
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const reset = () => dispatch({ type: "RESET" });
+
+  const toggle = () => {
+    if (state.timerState === "paused" || state.timerState === "stopped") {
+      dispatch({ type: "PLAY" });
+    } else if (state.timerState === "playing") {
+      dispatch({ type: "PAUSE" });
+    }
+  };
+
+  return (
+    <div className="h-screen flex justify-center max-w-7xl bg-white">
+      <div className="flex flex-col text-4xl my-10 h-96 w-1/2 border-2 bg-blue-900 border-gray-500">
+        <Display minutes={100} seconds={2} />
+        <Controls state={state} dispatch={dispatch} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/PlayButton.tsx
@@ -0,0 +1,20 @@
+export const PlayButton = () => {
+  return (
+    <div className="bg-white group rounded-full border p-5 border-gray-600 active:bg-gray-300 mr-5">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className=" w-10 h-10 fill-green-300 group-hover:fill-green-500"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+        />
+      </svg>
+    </div>
+  );
+};
